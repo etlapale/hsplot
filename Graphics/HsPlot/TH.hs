91ceab0f036e0413e5106a3730e76b0c6b2cdafc
Émilien Tlapale <emilien@tlapale.com>
2013-10-03 13:40:58 -0700

    Generating FromRecord with Template Haskell.

--- Graphics/HsPlot/TH.hs
+++ Graphics/HsPlot/TH.hs
@@ -1,17 +1,19 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
 
 module Graphics.HsPlot.TH (
-  deriveFromField, deriveFromRecord, enumToField, nameToField
+  deriveFromField, deriveFromRecordOffset, enumToField, nameToField
 )
 where
 
-import Control.Applicative (pure)
+import Control.Applicative ((<$>), (<*>), pure)
 import Control.Monad (mzero)
 import Data.List (elemIndex)
 import Language.Haskell.TH
 import System.IO.Unsafe
 
-import Data.Csv (Field, FromField, Parser, parseField)
+import qualified Data.Vector as V
+
+import Data.Csv ((.!), Field, FromField, FromRecord, Parser, parseField, parseRecord)
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BS8
@@ -27,29 +29,40 @@ enumToField ns n =
 nameToField :: Name -> Exp
 nameToField n = AppE (VarE 'BS8.pack) (LitE (StringL $ nameBase n))
 
-data T1
-
 deriveFromField :: Name -> Q [Dec]
 deriveFromField t = do
   TyConI (DataD _ _ _ cs _) <- reify t
-  let names = ListE $ map (\(NormalC n _) -> nameToField n) cs
-      etf = AppE (VarE 'enumToField) names
-  d <- [d| instance FromField T1 where
-             parseField = enumToField []
-       |]
-  let [InstanceD [] (AppT s (ConT _)) [ValD p _ []]] = d
-  return [InstanceD [] (AppT s (ConT t)) [ValD p (NormalB etf) []]]
+  let clauses = map makeClause cs ++ [wildClause]
+  return [InstanceD [] (AppT (ConT ''FromField) (ConT t))
+                        [FunD 'parseField clauses]]
+  where makeClause (NormalC n _) =
+          Clause [LitP (StringL $ nameBase n)]
+                 (NormalB (AppE (VarE 'pure) (ConE n)))
+                 []
+        wildClause = Clause [WildP] (NormalB (VarE 'mzero)) []
 
-deriveFromRecord :: Name -> Q [Dec]
-deriveFromRecord t = do
-  TyConI (DataD _ _ _ [RecC _ fs] _) <- reify t
+deriveFromRecordOffset :: Name -> Integer -> Q [Dec]
+deriveFromRecordOffset t o = do
+  TyConI (DataD _ _ _ [RecC t' fs] _) <- reify t
   -- Make sure every field is an instance of FromField
   ffs <- mapM ensureFromField fs
-  -- Make the record an instance of FromRecord
-  return $ concat ffs
+  -- Create the FromRecord instance
+  v <- newName "v"
+  let recLen = fromIntegral $ length fs
+  let infixE = buildInfix t' v (recLen - o)
+      d = InstanceD [] (AppT (ConT ''FromRecord) (ConT t))
+                       [FunD 'parseRecord [Clause [VarP v] (GuardedB [normalGuard, badLengthGuard]) []]]
+      normalGuard = (NormalG (InfixE (Just (AppE (VarE 'V.length) (VarE v))) (VarE '(==)) (Just (LitE (IntegerL $ recLen + o)))), infixE)
+      badLengthGuard = (NormalG (VarE 'otherwise), VarE 'mzero)
+  return $ concat $ ffs ++ [[d]]
   where ensureFromField (_,_,ConT n) = do
           isinst <- isInstance ''FromField [ConT n]
           if isinst
           then return []
           else deriveFromField n
         ensureFromField _ = return []  -- Not yet supported
+        buildInfix t v 0 = InfixE (Just (ConE t)) (VarE '(<$>)) $ buildJust v 0
+        buildInfix t v i = InfixE (Just $ buildInfix t v (i-1)) (VarE '(<*>)) $ buildJust v i
+        buildJust v i  = Just (InfixE (Just (VarE v))
+                                      (VarE '(.!))
+                                      (Just (LitE (IntegerL $ i + o))))
--- test.hs
+++ test.hs
@@ -2,7 +2,6 @@
 
 import Control.Applicative
 import Control.Monad
-import Language.Haskell.TH
 
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.Vector as V
@@ -13,14 +12,8 @@ import Graphics.HsPlot
 import Graphics.HsPlot.TH
 
 data Cut = Fair | Good | VeryGood | Premium | Ideal
-  deriving Enum
-
 data Colour = D | E | F | G | H | I | J
-  deriving (Enum)
-
 data Clarity = I3 | I2 | I1 | SI2 | SI1 | VS2 | VS1 | VVS2 | VVS1 | IF | FL
-  deriving Enum
-
 data Diamond = Diamond { carat :: Double
                        , cut :: Cut
                        , colour' :: Colour
@@ -32,11 +25,7 @@ data Diamond = Diamond { carat :: Double
                        , y' :: Double
                        , z' :: Double
                        }
-$(deriveFromRecord ''Diamond)
-
-instance FromRecord Diamond where
-  parseRecord v | V.length v == 11 = Diamond <$> v.!1 <*> v.!2 <*> v.!3 <*> v.!4 <*> v.!5 <*> v.!6 <*> v.!7 <*> v.!8 <*> v.!9 <*> v.!10
-                | otherwise = mzero
+$(deriveFromRecordOffset ''Diamond 1)
 
 main = do
   Right v <- decode True <$> BS.readFile "dsmall.csv"
