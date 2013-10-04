9e4a7eec5602d4a5fa8aa7acfdd4de4c0efeecec
Émilien Tlapale <emilien@tlapale.com>
2013-10-03 22:46:04 -0700

    Generalising nice range algorithms.

--- Graphics/HsPlot/Algorithms.hs
+++ Graphics/HsPlot/Algorithms.hs
@@ -14,26 +14,47 @@ module Graphics.HsPlot.Algorithms (
 )
 where
 
+class NiceNum a where
+  niceDiv :: a -> a -> a
+  niceNum :: a -> a
+  niceFloor :: RealFrac f => f -> a
+  niceCeiling :: RealFrac f => f -> a
 
--- |Return a nice number approximatively equal to x.
-niceNum :: Bool -> Double -> Double
-niceNum round x = niceFraction * (10 ^^ exponent)
-  where exponent = floor $ logBase 10 x
-        fraction = x / (10 ^^ exponent)
-        niceFraction | round && fraction < 1.5 = 1
-                     | round && fraction < 3 = 2
-                     | round && fraction < 7 = 5
-                     | round && fraction >= 7 = 10
-                     | fraction <= 1 = 1
-                     | fraction <= 2 = 2
-                     | fraction <= 5 = 5
-                     | otherwise = 10
+instance NiceNum Double where
+  niceDiv = (/)
+  niceNum = niceFloating
+  niceFloor = realToFrac . floor
+  niceCeiling = realToFrac . ceiling
+
+instance NiceNum Int where
+  niceDiv = div
+  niceNum = niceInteger
+  niceFloor = floor
+  niceCeiling = ceiling
+
+niceFloating :: (Fractional a, Real a) => a -> a
+niceFloating x = niceFrac * (10 ^^ expo)
+  where expo = floor $ logBase 10 (realToFrac x)
+        frac = (realToFrac x) / (10 ^^ expo)
+        niceFrac | frac < 1.5 = 1
+                 | frac < 3   = 2
+                 | frac < 7   = 5
+                 | otherwise  = 10
+
+niceInteger :: Real a => a -> a
+niceInteger x = niceFrac * (10 ^ expo)
+  where expo = floor $ logBase 10 (realToFrac x)
+        frac = (realToFrac x) / (10 ^^ expo)
+        niceFrac | frac <= 1 = 1
+                 | frac <= 2 = 2
+                 | frac <= 5 = 5
+                 | otherwise = 10
 
 -- |Return a nice range and tick spacing for a given interval.
-niceRange :: Double -> Double -> (Double,Double,Double)
-niceRange min max = (niceMin,niceMax,tickSpacing)
-  where tickSpacing = niceNum True $ range / realToFrac (maxTicks - 1)
-        range = niceNum False $ max - min
-        niceMin = tickSpacing * realToFrac (floor (min / tickSpacing))
-        niceMax = tickSpacing * realToFrac (ceiling (max / tickSpacing))
+niceRange :: (RealFrac a, NiceNum a) => a -> a -> (a,a,a)
+niceRange min max = (niceMin,niceMax,spacing)
+  where niceMin = spacing * niceFloor (realToFrac min / realToFrac spacing)
+        niceMax = spacing * niceCeiling (realToFrac max / realToFrac spacing)
+        spacing = niceNum $ range `niceDiv` (maxTicks - 1) 
+        range = niceInteger (max - min)
         maxTicks = 6
